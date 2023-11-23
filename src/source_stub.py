09567515e8448f0666c6e39b2a6660b2aaf8da95
Tuukka Yildirim <tuukka.yildirim@gmail.com>
2023-11-23 13:05:07 +0200

    reference bookin tilalle

--- src/source_stub.py
+++ src/source_stub.py
@@ -1,10 +1,10 @@
-from source_constructor import Book
+from reference import Reference
 
 def SourceStub():
     lähteet = []
     
     for i in range(5):
-        kirja = Book(f'Kirjailija_{i}', f'Kirja_{i}', f'Julkaisija_{i}', f'{i}')
+        kirja = Reference(f'Kirjailija_{i}', f'Kirja_{i}', f'Julkaisija_{i}', f'{i}')
         lähteet.append(kirja)
     
     return lähteet
