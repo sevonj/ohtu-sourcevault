187974bb47c30e564a7b664c5248babfcdeb7d98
Valtteri Ahola <valtteri.ahola@helsinki.fi>
2023-11-30 11:22:55 +0200

    cloud communication integrated into main program

--- src/index.py
+++ src/index.py
@@ -1,24 +1,27 @@
 """Moduuli joka luo ohjelman käyttäjälle"""
+from dotenv import dotenv_values
 from writer import Writer
 from app_ui import AppUI
 from root import Root
 from database_handler import Database
 from sql_server import ServerHandler
 
-#from server_handler import ServerHandler
-#url = "www.osoite.com"
-#server_handler = ServerHandler(url)
-#server_handler.get_refs()
-my_database_handler = ServerHandler()
-my_database_handler.start_up()
+env_config = dotenv_values(".env")
+aws_key = env_config["AWS_ACCESS_KEY_ID"]
+aws_secret_key = env_config["AWS_SECRET_ACCESS_KEY"]
 
-
-app_database = Database('newdb.db')#"database.db")
+app_cloud_database = ServerHandler(
+    aws_key,
+    aws_secret_key,
+    "database.db",
+    "tietokanta.db",
+    "sourcevaultbucket"
+)
+app_database = Database("database.db")
 app_writer = Writer("data.bib")
-app_root = Root(app_database, app_writer)
+app_root = Root(app_database, app_writer, app_cloud_database)
 
 ui = AppUI(app_root)
 
 if __name__ == "__main__":
     ui.run_app()
-my_database_handler.end_session()
--- src/sql_server.py
+++ src/sql_server.py
@@ -1,28 +1,42 @@
 import boto3
-import sqlite3
-from os import getenv
-from database_handler import Database
-from reference import Reference
+
 
 class ServerHandler:
-    def __init__(self):
-        print(getenv("AWS_ACCESS_KEY_ID"))
+    """
+    Kommunikoi AWS-pilvipalvelun kanssa.
+    ...
+
+    Parameters
+    ----------
+    aws_key, aws_secret_key : str
+        Avaimet jotka vaaditaan pilvipalvelun kanssa kommunikointiin.
+    local_db : str
+        Minne tallennetaan.
+    remote_db : str
+        Tietokannan nimi pilvipalvelussa.
+    bucket_name : str
+        AWS-bucket, johon tietokanta on tallennettu.
+
+    Methods
+    -------
+    get_references():
+        Hakee tietokannan AWS-pilvipalvelusta.
+    upload_references():
+        Tallentaa tietokannan AWS-pilvipalveluun.
+    """
+    def __init__(self, aws_key, aws_secret_key, local_db, remote_db, bucket_name):
+        self.local_db = local_db
+        self.remote_db = remote_db
+        self.bucket_name = bucket_name
         self.s3 = boto3.client(
             's3',
-            aws_access_key_id="key",
-            aws_secret_access_key="key",
+            aws_access_key_id=aws_key,
+            aws_secret_access_key=aws_secret_key,
             region_name='eu-north-1'
             )
         
+    def get_references(self):
+        self.s3.download_file(self.bucket_name, self.remote_db, self.local_db)
 
-    def get_references(self, bucketname:str, remote_db_name:str, local_path:str):
-        self.s3.download_file(bucketname, remote_db_name, local_path)
-
-    def upload_references(self, local_path:str, bucketname:str, remote_db_name:str):
-        self.s3.upload_file(local_path, bucketname, remote_db_name)
-    
-    def start_up(self):
-        self.get_references("sourcevaultbucket", "tietokanta.db", "newdb.db")
-    
-    def end_session(self):
-        self.upload_references("newdb.db", "sourcevaultbucket", "tietokanta.db")
+    def upload_references(self):
+        self.s3.upload_file(self.local_db, self.bucket_name, self.remote_db)
